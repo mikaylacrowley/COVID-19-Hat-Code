29068706696c6430345e25ed110376a958ca5a94
mikaylacrowley <82425804+mikaylacrowley@users.noreply.github.com>
2021-04-12 21:57:39 -0400

    Add files via upload

---
+++ UltraSonic_Sensor.ino
@@ -0,0 +1,62 @@
+#define trigPin 6
+#define echoPin 5
+#define trigPin2 3
+#define echoPin2 2
+#define led 11
+#define led2 10
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+
+  long duration2, distance2;
+  digitalWrite(trigPin2, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin2, HIGH);
+  //delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin2, LOW);
+  duration2 = pulseIn(echoPin2, HIGH);
+  distance2 = (duration2/2) / 29.1;
+  
+  if (distance < 35 || distance2 < 35) {  // This is where the LED On/Off happens
+  digitalWrite(led,HIGH);// When the Red condition is met, the Green LED should turn off
+  digitalWrite(led2,HIGH);
+  tone (8, 2000, 300);
+}
+
+  else {
+    digitalWrite(led,LOW);
+    digitalWrite(led2,LOW);
+    
+  }
+  if (distance >= 200 || distance <= 0){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm ");
+
+    Serial.print(distance2);
+    Serial.println(" cm ");
+    
+  }
+  delay(500);
+}
